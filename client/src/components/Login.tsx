854f1e1d19741919efd01038cb05a4caf9215bbe
Brian <bbbrianboylen@gmail.com>
2021-05-17 22:15:23 -0500

    client fetches user

--- client/src/components/Login.tsx
+++ client/src/components/Login.tsx
@@ -2,7 +2,36 @@ import React, { useState, useEffect } from "react";
 import { Card, Button, Row, Col } from "antd";
 
 const Login: React.FC = () => {
-  const [data, setData] = useState(null);
+  // user interface
+
+  const [authenticated, setAuthenticated] = useState<Boolean>(false);
+  const [user, setUser] = useState<Object>(false);
+
+  useEffect(() => {
+    const authenticateUser = async () => {
+      await fetch("http://localhost:3001/auth/login/success", {
+        method: "GET",
+        credentials: "include",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+      })
+        .then((response) => {
+          if (response.status === 200) return response.json();
+          throw new Error("failed to authenticate user");
+        })
+        .then((responseJson) => {
+          setAuthenticated(true);
+          setUser(responseJson.user);
+        })
+        .catch((error) => {
+          setAuthenticated(false);
+          throw new Error("failed 2 authenticate user");
+        });
+    }
+    authenticateUser();
+  }, []);
 
   const loginToSpotify = (e: any) => {
     //What do I add here?
@@ -12,20 +41,26 @@ const Login: React.FC = () => {
 
   return (
     <div className="Login">
-      <Row justify={"center"} style={{ paddingTop: "200px" }}>
-        <Col>
-          <Card
-            title="Login to Symphony"
-            style={{ width: 300, textAlign: "center" }}
-            headStyle={{ backgroundColor: "salmon" }}
-          >
-            <Button onClick={loginToSpotify} type={"primary"}>
-              Login with Spotify
-            </Button>
-            <Button style={{ marginTop: "25px" }}>Login as guest</Button>
-          </Card>
-        </Col>
-      </Row>
+      {authenticated ? (
+        <h1> Welcome</h1>
+      ) : (
+        <Row justify={"center"} style={{ paddingTop: "200px" }}>
+          <Col>
+            <Card
+              title="Login to Symphony"
+              style={{ width: 300, textAlign: "center" }}
+              headStyle={{ backgroundColor: "salmon" }}
+            >
+              <Button onClick={loginToSpotify} type={"primary"}>
+                Login with Spotify
+              </Button>
+              <Button type={"default"} style={{ marginTop: "25px" }}>
+                Login as guest
+              </Button>
+            </Card>
+          </Col>
+        </Row>
+      )}
     </div>
   );
 };
--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ const passport = require("passport");
 const isLoggedIn = require("./middleware/auth.ts");
 const connectDB = require("./config/db");
 const authRoutes = require("./routes/auth-routes");
+const cors = require('cors');
 require("./utilities/passport.ts");
 
 const PORT = process.env.PORT || 3001;
@@ -24,13 +25,13 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 // set up cors to allow us to accept requests from our client
-// app.use(
-//   cors({
-//     origin: "http://localhost:3000", // allow to server to accept request from different origin
-//     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-//     credentials: true // allow session cookie from browser to pass through
-//   })
-// );
+app.use(
+  cors({
+    origin: "http://localhost:3000", 
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true 
+  })
+);
 
 // Should this return 401 failure if not logged in?
 app.get("/", isLoggedIn, (req, res) => {
