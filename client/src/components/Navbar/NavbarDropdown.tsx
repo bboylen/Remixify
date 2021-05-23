cd8af2520bd4bad157747c237d135782cf85ffa6
Brian <bbbrianboylen@gmail.com>
2021-05-23 17:20:01 -0500

    dropdown menu improved

--- client/src/components/Navbar/NavbarDropdown.tsx
+++ client/src/components/Navbar/NavbarDropdown.tsx
@@ -1,41 +1,52 @@
-import React from "react";
+import React, { CSSProperties } from "react";
 import { Link } from "react-router-dom";
 import { Menu, Dropdown } from "antd";
 
 interface Props {
-  location: any
+  location: any;
 }
 
 export const NavbarDropdown: React.FC<any> = (props) => {
   const { location } = props;
 
+  const itemStyle: CSSProperties = {
+    width: '100vw',
+    height: '35px',
+    textAlign: 'center',
+    fontSize: '17px',
+  };
+  
   const menu = (
-<Menu
+    <Menu
       id="header-nav"
       style={{
-        
+        marginTop: '17px',
+        padding: '0px',
       }}
       theme="dark"
       mode="horizontal"
       defaultSelectedKeys={["/listen"]}
       selectedKeys={[location.pathname]}
     >
-      <Menu.Item key="/listen">
+      <Menu.Item  style={itemStyle} key="/listen">
         <Link to="/listen">Listen</Link>
       </Menu.Item>
-      <Menu.Item key="/playlists">
+      <Menu.Item style={itemStyle} key="/playlists">
         <Link to="/playlists">Playlists</Link>
       </Menu.Item>
     </Menu>
-  )
+  );
   return (
-    <Dropdown overlay={menu} trigger={['click']}>
-    <a className="ant-dropdown-link" onClick={e => e.preventDefault()} style={{
-      marginLeft: '10%',
-
-    }}>
-      Menu
-    </a>
-  </Dropdown>
+    <Dropdown overlay={menu} trigger={["click"]}>
+      <a
+        className="ant-dropdown-link"
+        onClick={(e) => e.preventDefault()}
+        style={{
+          marginLeft: "10%",
+        }}
+      >
+        Menu
+      </a>
+    </Dropdown>
   );
 };
