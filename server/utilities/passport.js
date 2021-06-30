78503d5046bdb82889fc4b275d0dcfcd8b63f69b
Brian <bbbrianboylen@gmail.com>
2021-06-29 20:10:01 -0500

    change to spotify auth callback url

--- server/utilities/passport.js
+++ server/utilities/passport.js
@@ -1,6 +1,10 @@
 const passport = require("passport");
 const SpotifyStrategy = require("passport-spotify").Strategy;
 const User = require("../models/user-model");
+const CLIENT_HOME_PAGE_URL =
+  process.env.NODE_ENV === "production"
+    ? "https://still-peak-57686.herokuapp.com"
+    : "http://localhost:3001";
 
 passport.serializeUser(function (user, done) {
   done(null, user.id);
@@ -21,7 +25,7 @@ passport.use(
     {
       clientID: process.env.SPOTIFY_ID,
       clientSecret: process.env.SPOTIFY_SECRET,
-      callbackURL: "/auth/spotify/callback",
+      callbackURL: `${CLIENT_HOME_PAGE_URL}/auth/spotify/callback`,
     },
     async (accessToken, refreshToken, profile, done) => {
       const currentUser = await User.findOne({
